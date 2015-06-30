43b91d1e00a3a9775e287daadab0cb0f02a690fe
pknam <pknam.kr@gmail.com>
2015-06-30 14:35:36 +0900

    board in/out 구분

--- Assets/scripts/bezierPointBehaviour.cs
+++ Assets/scripts/bezierPointBehaviour.cs
@@ -6,5 +6,10 @@ public class bezierPointBehaviour : MonoBehaviour
     void OnMouseDrag()
     {
         transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
+
+        if (GameObject.FindGameObjectWithTag("board").GetComponent<BoxCollider2D>().bounds.Contains(new Vector3(transform.position.x, transform.position.y, 1)))
+            Debug.Log("in board");
+        else
+            Debug.Log("out board");
     }
 }
