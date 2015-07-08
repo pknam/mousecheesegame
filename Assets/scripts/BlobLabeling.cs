c79d52bd00cc913bf75ee2a395c52b6bc904846e
pknam <pknam.kr@gmail.com>
2015-07-08 17:56:07 +0900

    blob labeling 코드 포팅

---
+++ Assets/scripts/BlobLabeling.cs
@@ -0,0 +1,323 @@
+﻿using UnityEngine;
+using System.Collections;
+
+class Point
+{
+    public int x;
+    public int y;
+
+    public Point(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Visited
+{
+    public bool visitedFlag;
+    public Point point;
+}
+
+class BlobLabeling : Object
+{
+    public Color[] m_image;
+    public int m_nWidth;
+    public int m_nHeight;
+
+    // labeling을위한 buffer
+    public int[] m_cdataBuf;
+
+    public int m_nThreshold;
+    public Visited[] m_vPoint;
+    public int m_nBlobs;
+    public Rect[] m_recBlobs;
+
+    private const int MAX_BLOBS = 10000;
+    private const int MAX_LABEL = 100;
+
+    public BlobLabeling()
+    {
+        m_nThreshold = 0;
+        m_nBlobs = MAX_BLOBS;
+        m_image = null;
+        m_recBlobs = null;
+    }
+
+    public void setParam(Color[] image, int nWidth, int nHeight, int nThreshold)
+    {
+        if (m_recBlobs != null)
+        {
+            m_recBlobs = null;
+            m_nBlobs = MAX_BLOBS;
+        }
+
+        m_image = image.Clone() as Color[];
+        m_nWidth = nWidth;
+        m_nHeight = nHeight;
+        m_nThreshold = nThreshold;
+    }
+
+    public void DoLabeling()
+    {
+        m_nBlobs = Labeling(m_image, m_nThreshold);
+    }
+
+    private int Labeling(Color[] image, int nThreshold)
+    {
+        int nNumber;
+
+        m_cdataBuf = new int[m_nWidth * m_nHeight];
+
+        for (int j = 0; j < m_nHeight; j++)
+            for (int i = 0; i < m_nWidth; i++)
+                if (image[j * m_nWidth + i].Equals(new Color(1, 1, 1)))
+                    m_cdataBuf[j * m_nWidth + i] = 255;
+
+        initvPoint(m_nWidth, m_nHeight);
+
+        // labeling
+        nNumber = _Labeling(m_nWidth, m_nHeight, nThreshold);
+
+
+        if (nNumber != MAX_BLOBS)
+            m_recBlobs = new Rect[nNumber];
+
+        if (nNumber != 0)
+            detectLabelingRegion(nNumber, m_nWidth, m_nHeight);
+
+        return nNumber;
+    }
+
+    private void initvPoint(int nWidth, int nHeight)
+    {
+        int nX, nY;
+
+        m_vPoint = new Visited[nWidth * nHeight];
+
+        for (nY = 0; nY < nHeight; nY++)
+        {
+            for (nX = 0; nX < nWidth; nX++)
+            {
+                m_vPoint[nY * nWidth + nX] = new Visited();
+                m_vPoint[nY * nWidth + nX].visitedFlag = false;
+                m_vPoint[nY * nWidth + nX].point = new Point(nX, nY);
+            }
+        }
+    }
+
+
+    private void detectLabelingRegion(int nLabelNumber, int nWidth, int nHeight)
+    {
+        int nX, nY;
+        int nLabelIndex;
+
+        bool[] bFirstFlag = new bool[255];
+
+        for (int i = 0; i < bFirstFlag.Length; i++)
+            bFirstFlag[i] = false;
+
+        for (nY = 1; nY < nHeight - 1; nY++)
+        {
+            for (nX = 1; nX < nWidth - 1; nX++)
+            {
+                nLabelIndex = m_cdataBuf[nY * nWidth + nX];
+
+                if (nLabelIndex != 0)	// Is this a new component?, 255 == Object
+                {
+                    if (bFirstFlag[nLabelIndex] == false)
+                    {
+                        m_recBlobs[nLabelIndex - 1].x = nX;
+                        m_recBlobs[nLabelIndex - 1].y = nY;
+                        m_recBlobs[nLabelIndex - 1].width = 0;
+                        m_recBlobs[nLabelIndex - 1].height = 0;
+
+                        bFirstFlag[nLabelIndex] = true;
+                    }
+                    else
+                    {
+                        int left = (int)m_recBlobs[nLabelIndex - 1].x;
+                        int right = left + (int)m_recBlobs[nLabelIndex - 1].width;
+                        int top = (int)m_recBlobs[nLabelIndex - 1].y;
+                        int bottom = top + (int)m_recBlobs[nLabelIndex - 1].height;
+
+                        if (left >= nX) left = nX;
+                        if (right <= nX) right = nX;
+                        if (top >= nY) top = nY;
+                        if (bottom <= nY) bottom = nY;
+
+                        m_recBlobs[nLabelIndex - 1].x = left;
+                        m_recBlobs[nLabelIndex - 1].y = top;
+                        m_recBlobs[nLabelIndex - 1].width = right - left;
+                        m_recBlobs[nLabelIndex - 1].height = bottom - top;
+
+                    }
+                }
+
+            }
+        }
+    }
+
+    private int _Labeling(int nWidth, int nHeight, int nThreshold)
+    {
+        int num = 0;
+        int nX, nY;
+        int startX, startY, endX, endY;
+
+        for (nY = 0; nY < nHeight; nY++)
+        {
+            for (nX = 0; nX < nWidth; nX++)
+            {
+                if (m_cdataBuf[nY * nWidth + nX] == 255)
+                {
+                    num++;
+                    m_cdataBuf[nY * nWidth + nX] = num;
+
+                    startX = nX;
+                    startY = nY;
+                    endX = nX;
+                    endY = nY;
+
+                    __NRFindNeighbor(nWidth, nHeight, nX, nY, ref startX, ref startY, ref endX, ref endY);
+
+                    if (__Area(startX, startY, endX, endY, nWidth, num) != 0)
+                    {
+                        for (int k = startY; k <= endY; k++)
+                        {
+                            for (int l = startX; l <= endX; l++)
+                            {
+                                if (m_cdataBuf[k * nWidth + l] == num)
+                                    m_cdataBuf[k * nWidth + l] = 0;
+                            }
+                        }
+
+                        num--;
+
+                        if (num > 250)
+                            return 0;
+                    }
+                }
+            }
+        }
+
+
+        return num;
+    }
+
+    private int __NRFindNeighbor(int nWidth, int nHeight, int nPosX, int nPosY, ref int startX, ref int startY, ref int endX, ref int endY)
+    {
+        Point CurrentPoint = new Point(nPosX, nPosY);
+
+        m_vPoint[CurrentPoint.y * nWidth + CurrentPoint.x].visitedFlag = true;
+        m_vPoint[CurrentPoint.y * nWidth + CurrentPoint.x].point.x = nPosX;
+        m_vPoint[CurrentPoint.y * nWidth + CurrentPoint.x].point.y = nPosY;
+
+
+        while (true)
+        {
+            if ((CurrentPoint.x != 0) && (m_cdataBuf[CurrentPoint.y * nWidth + CurrentPoint.x - 1] == 255))   // -X 방향
+            {
+                if (m_vPoint[CurrentPoint.y * nWidth + CurrentPoint.x - 1].visitedFlag == false)
+                {
+                    m_cdataBuf[CurrentPoint.y * nWidth + CurrentPoint.x - 1] = m_cdataBuf[CurrentPoint.y * nWidth + CurrentPoint.x];	// If so, mark it
+                    m_vPoint[CurrentPoint.y * nWidth + CurrentPoint.x - 1].visitedFlag = true;
+                    m_vPoint[CurrentPoint.y * nWidth + CurrentPoint.x - 1].point = CurrentPoint;
+                    CurrentPoint.x--;
+
+                    if (CurrentPoint.x <= 0)
+                        CurrentPoint.x = 0;
+
+                    if (startX >= CurrentPoint.x)
+                        startX = CurrentPoint.x;
+
+                    continue;
+                }
+            }
+
+            if ((CurrentPoint.x != nWidth - 1) && (m_cdataBuf[CurrentPoint.y * nWidth + CurrentPoint.x + 1] == 255))   // -X 방향
+            {
+                if (m_vPoint[CurrentPoint.y * nWidth + CurrentPoint.x + 1].visitedFlag == false)
+                {
+                    m_cdataBuf[CurrentPoint.y * nWidth + CurrentPoint.x + 1] = m_cdataBuf[CurrentPoint.y * nWidth + CurrentPoint.x];	// If so, mark it
+                    m_vPoint[CurrentPoint.y * nWidth + CurrentPoint.x + 1].visitedFlag = true;
+                    m_vPoint[CurrentPoint.y * nWidth + CurrentPoint.x + 1].point = CurrentPoint;
+                    CurrentPoint.x++;
+
+                    if (CurrentPoint.x >= nWidth - 1)
+                        CurrentPoint.x = nWidth - 1;
+
+                    if (endX <= CurrentPoint.x)
+                        endX = CurrentPoint.x;
+
+                    continue;
+                }
+            }
+
+            if ((CurrentPoint.y != 0) && (m_cdataBuf[(CurrentPoint.y - 1) * nWidth + CurrentPoint.x] == 255))   // -X 방향
+            {
+                if (m_vPoint[(CurrentPoint.y - 1) * nWidth + CurrentPoint.x].visitedFlag == false)
+                {
+                    m_cdataBuf[(CurrentPoint.y - 1) * nWidth + CurrentPoint.x] = m_cdataBuf[CurrentPoint.y * nWidth + CurrentPoint.x];	// If so, mark it
+                    m_vPoint[(CurrentPoint.y - 1) * nWidth + CurrentPoint.x].visitedFlag = true;
+                    m_vPoint[(CurrentPoint.y - 1) * nWidth + CurrentPoint.x].point = CurrentPoint;
+                    CurrentPoint.y--;
+
+                    if (CurrentPoint.y <= 0)
+                        CurrentPoint.y = 0;
+
+                    if (startY >= CurrentPoint.y)
+                        startY = CurrentPoint.y;
+
+                    continue;
+                }
+            }
+
+            if ((CurrentPoint.y != nHeight - 1) && (m_cdataBuf[(CurrentPoint.y + 1) * nWidth + CurrentPoint.x] == 255))   // -X 방향
+            {
+                if (m_vPoint[(CurrentPoint.y + 1) * nWidth + CurrentPoint.x].visitedFlag == false)
+                {
+                    m_cdataBuf[(CurrentPoint.y + 1) * nWidth + CurrentPoint.x] = m_cdataBuf[CurrentPoint.y * nWidth + CurrentPoint.x];	// If so, mark it
+                    m_vPoint[(CurrentPoint.y + 1) * nWidth + CurrentPoint.x].visitedFlag = true;
+                    m_vPoint[(CurrentPoint.y + 1) * nWidth + CurrentPoint.x].point = CurrentPoint;
+                    CurrentPoint.y++;
+
+                    if (CurrentPoint.y >= nHeight - 1)
+                        CurrentPoint.y = nHeight - 1;
+
+                    if (endY <= CurrentPoint.y)
+                        endY = CurrentPoint.y;
+
+                    continue;
+                }
+            }
+
+            if ((CurrentPoint.x == m_vPoint[CurrentPoint.y * nWidth + CurrentPoint.x].point.x)
+                && (CurrentPoint.y == m_vPoint[CurrentPoint.y * nWidth + CurrentPoint.x].point.y))
+            {
+                break;
+            }
+            else
+            {
+                CurrentPoint = m_vPoint[CurrentPoint.y * nWidth + CurrentPoint.x].point;
+            }
+        }
+
+        return 0;
+    }
+
+    private int __Area(int startX, int startY, int endX, int endY, int nWidth, int nLevel)
+    {
+        int nArea = 0;
+
+        for (int nY = startY; nY < endY; nY++)
+        {
+            for (int nX = startX; nX < endX; nX++)
+            {
+                if (m_cdataBuf[nY * nWidth + nX] == nLevel)
+                    nArea++;
+            }
+        }
+
+        return nArea;
+    }
+}
--- Assets/scripts/WebCam.cs
+++ Assets/scripts/WebCam.cs
@@ -8,9 +8,33 @@ public class WebCam : MonoBehaviour
     private WebCamTexture webcamTexture;
     Texture2D viewTexture;
 
+    BlobLabeling m_blobLabeling;
+
+    [Range(0, 1)]
+    public float h_upper;
+    [Range(0, 1)]
+    public float h_lower;
+    [Range(0, 1)]
+    public float s_upper;
+    [Range(0, 1)]
+    public float s_lower;
+    [Range(0, 1)]
+    public float v_upper;
+    [Range(0, 1)]
+    public float v_lower;
+
     // Use this for initialization
     void Start()
     {
+        h_upper = 0.995f;
+        h_lower = 0.938f;
+        s_upper = 0.88f;
+        s_lower = 0.673f;
+        v_upper = 0.706f;
+        v_lower = 0.544f;
+
+        m_blobLabeling = new BlobLabeling();
+
         // resolusion : 640 * 480
         // FPS : 30
         //webcamTexture = new WebCamTexture(WebCamTexture.devices[0].name, 640, 480, 30);
@@ -38,9 +62,11 @@ public class WebCam : MonoBehaviour
         float h, s, v;
         for (int i = 0; i < cam_pixels.Length; i++)
         {
-            EditorGUIUtility.RGBToHSV(cam_pixels[i], out h, out s, out v);
+            RGBToHSV(cam_pixels[i], out h, out s, out v);
 
-            if (h < 0.6f && h > 0.5f)
+            if (h_lower <= h && h <= h_upper &&
+                s_lower <= s && s <= s_upper &&
+                v_lower <= v && v <= v_upper)
                 view_pixels[i] = new Color(1, 1, 1);
             else
                 view_pixels[i] = new Color(0, 0, 0);
@@ -48,7 +74,72 @@ public class WebCam : MonoBehaviour
 
         viewTexture.SetPixels(view_pixels);
         viewTexture.Apply();
+
+        m_blobLabeling.setParam(view_pixels, webcamTexture.width, webcamTexture.height, 50);
+        m_blobLabeling.DoLabeling();
+
+        Debug.Log(m_blobLabeling.m_nBlobs);
+
+        Vector3 viewPos = this.transform.position;
+        Bounds viewBounds = this.GetComponent<Renderer>().bounds;
+
+        for(int i=0; i<m_blobLabeling.m_nBlobs; i++)
+        {
+            Point pt1 = new Point((int)m_blobLabeling.m_recBlobs[i].x, (int)m_blobLabeling.m_recBlobs[i].y);
+            Point pt2 = new Point(pt1.x + (int)m_blobLabeling.m_recBlobs[i].width, pt1.y + (int)m_blobLabeling.m_recBlobs[i].height);
+
+            Debug.DrawLine(
+                new Vector3(
+                    pt1.x * viewBounds.size.x / webcamTexture.width + viewPos.x - viewBounds.size.x / 2,
+                    pt1.y * viewBounds.size.y / webcamTexture.height + viewPos.y - viewBounds.size.y / 2,
+                    -20),
+                new Vector3(
+                    pt2.x * viewBounds.size.x / webcamTexture.width + viewPos.x - viewBounds.size.x / 2,
+                    pt2.y * viewBounds.size.y / webcamTexture.height + viewPos.y - viewBounds.size.y / 2,
+                    -20),
+                Color.green);
+        }
     }
+
+    public static void RGBToHSV(Color rgbColor, out float H, out float S, out float V)
+    {
+        if ((double)rgbColor.b > (double)rgbColor.g && (double)rgbColor.b > (double)rgbColor.r)
+            RGBToHSVHelper(4f, rgbColor.b, rgbColor.r, rgbColor.g, out H, out S, out V);
+        else if ((double)rgbColor.g > (double)rgbColor.r)
+            RGBToHSVHelper(2f, rgbColor.g, rgbColor.b, rgbColor.r, out H, out S, out V);
+        else
+            RGBToHSVHelper(0.0f, rgbColor.r, rgbColor.g, rgbColor.b, out H, out S, out V);
+    }
+
+    private static void RGBToHSVHelper(float offset, float dominantcolor, float colorone, float colortwo, out float H, out float S, out float V)
+    {
+        V = dominantcolor;
+        if ((double)V != 0.0)
+        {
+            float num1 = (double)colorone <= (double)colortwo ? colorone : colortwo;
+            float num2 = V - num1;
+            if ((double)num2 != 0.0)
+            {
+                S = num2 / V;
+                H = offset + (colorone - colortwo) / num2;
+            }
+            else
+            {
+                S = 0.0f;
+                H = offset + (colorone - colortwo);
+            }
+            H = H / 6f;
+            if ((double)H >= 0.0)
+                return;
+            H = H + 1f;
+        }
+        else
+        {
+            S = 0.0f;
+            H = 0.0f;
+        }
+    }
+
 }
 
 //class HSV
