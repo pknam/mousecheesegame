c50a3693e1ee0ffad36a41e92389291434488541
pknam <pknam.kr@gmail.com>
2015-07-17 09:07:45 +0900

    camera matrix test

--- Assets/scripts/CamMatrixControl.cs
+++ Assets/scripts/CamMatrixControl.cs
@@ -5,6 +5,15 @@ public class CamMatrixControl : MonoBehaviour
 {
     private Camera cam;
 
+    [Range(0, 1)]
+    public float m00;
+    [Range(0, 1)]
+    public float m01;
+    [Range(0, 1)]
+    public float m10;
+    [Range(0, 1)]
+    public float m11;
+
     // Use this for initialization
     void Start()
     {
@@ -17,10 +26,16 @@ public class CamMatrixControl : MonoBehaviour
         //Debug.Log(cam.projectionMatrix);
 
         Matrix4x4 tmp = cam.projectionMatrix;
-        //tmp.m00 = Mathf.Sin(Time.time * 1.2f) * 1f;
-        tmp.m01 = Mathf.Sin(Time.time * 1.2f) * 0.5f;
-        tmp.m10 = Mathf.Sin(Time.time * 1.5f) * 0.5f;
-        //tmp.m11 = Mathf.Sin(Time.time * 1.2f) * 1f;
+        //tmp[0, 0] = Mathf.Sin(Time.time * 1.2f) * 1f;
+        //tmp[0, 1] = Mathf.Sin(Time.time * 1.2f) * 0.5f;
+        //tmp[1, 0] = Mathf.Sin(Time.time * 1.5f) * 0.5f;
+        //tmp[1, 1] = Mathf.Sin(Time.time * 1.2f) * 1f;
+
+        tmp[0, 0] = m00;
+        tmp[0, 1] = m01;
+        tmp[1, 0] = m10;
+        tmp[1, 1] = m11;
+
         cam.projectionMatrix = tmp;
     }
 }
