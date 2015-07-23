428bd280d2b865139da471884e2e80fd372490ba
pknam <pknam.kr@gmail.com>
2015-07-23 17:59:59 +0900

    3차원 회전

--- Assets/scripts/CamMatrixControl.cs
+++ Assets/scripts/CamMatrixControl.cs
@@ -3,12 +3,17 @@ using System.Collections;
 
 public class CamMatrixControl : MonoBehaviour
 {
-    public float rotate_angle = 0;
+    public float x_rotation = 0;
+    public float y_rotation = 0;
+    public float z_rotation = 0;
     public float scale = 1;
     public Vector2 shearing = Vector2.zero;
     public Vector2 translation = Vector2.zero;
 
-    private Matrix4x4 MatRotate = Matrix4x4.identity;
+
+    private Matrix4x4 MatXRotate = Matrix4x4.identity;
+    private Matrix4x4 MatYRotate = Matrix4x4.identity;
+    private Matrix4x4 MatZRotate = Matrix4x4.identity;
     private Matrix4x4 MatScale = Matrix4x4.identity;
     private Matrix4x4 MatXShearing = Matrix4x4.identity;
     private Matrix4x4 MatYShearing = Matrix4x4.identity;
@@ -30,10 +35,20 @@ public class CamMatrixControl : MonoBehaviour
         MatScale[0, 0] = scale;
         MatScale[1, 1] = scale;
 
-        MatRotate[0, 0] = Mathf.Cos(rotate_angle * Mathf.Deg2Rad);
-        MatRotate[0, 1] = -Mathf.Sin(rotate_angle * Mathf.Deg2Rad);
-        MatRotate[1, 0] = Mathf.Sin(rotate_angle * Mathf.Deg2Rad);
-        MatRotate[1, 1] = Mathf.Cos(rotate_angle * Mathf.Deg2Rad);
+        MatXRotate[1, 1] = Mathf.Cos(x_rotation * Mathf.Deg2Rad);
+        MatXRotate[1, 2] = -Mathf.Sin(x_rotation * Mathf.Deg2Rad);
+        MatXRotate[2, 1] = Mathf.Sin(x_rotation * Mathf.Deg2Rad);
+        MatXRotate[2, 2] = Mathf.Cos(x_rotation * Mathf.Deg2Rad);
+
+        MatYRotate[0, 0] = Mathf.Cos(y_rotation * Mathf.Deg2Rad);
+        MatYRotate[0, 2] = Mathf.Sin(y_rotation * Mathf.Deg2Rad);
+        MatYRotate[2, 0] = -Mathf.Sin(y_rotation * Mathf.Deg2Rad);
+        MatYRotate[2, 2] = Mathf.Cos(y_rotation * Mathf.Deg2Rad);
+
+        MatZRotate[0, 0] = Mathf.Cos(z_rotation * Mathf.Deg2Rad);
+        MatZRotate[0, 1] = -Mathf.Sin(z_rotation * Mathf.Deg2Rad);
+        MatZRotate[1, 0] = Mathf.Sin(z_rotation * Mathf.Deg2Rad);
+        MatZRotate[1, 1] = Mathf.Cos(z_rotation * Mathf.Deg2Rad);
 
         MatXShearing[0, 1] = shearing.x;
         MatYShearing[1, 0] = shearing.y;
@@ -43,7 +58,9 @@ public class CamMatrixControl : MonoBehaviour
 
         cam.projectionMatrix = OriMatProjection *
                                 MatScale *
-                                MatRotate *
+                                MatXRotate *
+                                MatYRotate *
+                                MatZRotate *
                                 MatXShearing *
                                 MatYShearing *
                                 MatTranslation;
